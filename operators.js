b52a74c61363067c846fabbb27b2c59a594012f7
sakshi pathare <sakshipathare304@gmail.com>
2025-12-31 12:28:41 +0530

    opeartors added

---
+++ operators.js
@@ -0,0 +1,34 @@
+let a = 10;
+let b = 5;
+
+// Arithmetic Operators
+console.log("Addition:", a + b);        
+console.log("Subtraction:", a - b);    
+console.log("Multiplication:", a * b); 
+console.log("Division:", a / b);       
+console.log("Modulus:", a % b);         
+
+// Increment and Decrement
+a++;
+b--;
+console.log("Increment a:", a); 
+console.log("Decrement b:", b); 
+
+// Comparison Operators
+console.log("a == '11':", a == "11");   
+console.log("a === '11':", a === "11"); 
+console.log("a != b:", a != b);         
+console.log("a > b:", a > b);         
+  console.log("a <= b:", a <= b);         
+
+// Logical Operators
+console.log("AND:", a > 5 && b < 10);  
+console.log("OR:", a < 5 || b < 10);    
+console.log("NOT:", !(a > b));        
+
+// Assignment Operators
+let x = 20;
+x += 5;
+x -= 3;
+x *= 2;
+x /= 2;
