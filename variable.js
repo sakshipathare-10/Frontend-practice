e7ea4eb5b647d5095208fd71c8a6d355bbde765b
sakshi pathare <sakshipathare304@gmail.com>
2025-12-31 11:24:28 +0530

    added

--- variable.js
+++ variable.js
@@ -1,2 +1,12 @@
-var x=10;
-console.log(x);
+
+let userName = "sakshi";
+let userAge = 22;
+
+console.log(userName); 
+console.log(userAge);  
+
+userAge = 31;
+console.log("New age:", userAge); 
+
+const appName = "MyAwesomeApp";
+const pi = 3.14;
