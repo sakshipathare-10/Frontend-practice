87233d2f6b4a6cf26dc2d0ff19fc202739650d3d
sakshi pathare <sakshipathare304@gmail.com>
2026-01-05 20:27:41 +0530

    form-added

--- Vite-react-app/react-jsx-demo/src/App.tsx
+++ Vite-react-app/react-jsx-demo/src/App.tsx
@@ -1,15 +1,28 @@
 function App() {
-  const fruits = ["Apple", "Mango", "Banana"];
-
   return (
-    <div>
-      <h2>Fruit List</h2>
+    <div style={{ padding: "20px" }}>
+      <h2>Login Form</h2>
 
-      <ul>
-        {fruits.map((fruit, index) => (
-          <li key={index}>{fruit}</li>
-        ))}
-      </ul>
+      <form>
+        {/* Username */}
+        <div>
+          <label>Username:</label><br />
+          <input type="text" placeholder="Enter username" />
+        </div>
+
+        <br />
+
+        {/* Password */}
+        <div>
+          <label>Password:</label><br />
+          <input type="password" placeholder="Enter password" />
+        </div>
+
+        <br />
+
+        {/* Button */}
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
